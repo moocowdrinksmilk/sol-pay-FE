52a4bc03334c0e468ae71eddc0a15ab8d6a093b2
Rui Xian <tayrx9988@gmail.com>
2022-03-31 23:18:58 +0800

    feat: 2nd commit

---
+++ components/WalletWrapper.tsx
@@ -0,0 +1,65 @@
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+    LedgerWalletAdapter,
+    PhantomWalletAdapter,
+    SlopeWalletAdapter,
+    SolflareWalletAdapter,
+    SolletExtensionWalletAdapter,
+    SolletWalletAdapter,
+    TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl, Connection } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+interface props {
+    children: any
+}
+
+const WalletWrapper = (props: props) => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+
+    const network = 'http://127.0.0.1:8899';
+    const connection = new Connection(network, {
+        commitment: 'confirmed'
+    });
+    
+    // You can also provide a custom RPC endpoint.
+
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded.
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            // new SlopeWalletAdapter(),
+            // new SolflareWalletAdapter({ network }),
+            // new TorusWalletAdapter(),
+            // new LedgerWalletAdapter(),
+            // new SolletWalletAdapter({ network }),
+            // new SolletExtensionWalletAdapter({ network }),
+        ],
+        [network]
+    );
+
+    return (
+        <ConnectionProvider endpoint={network}>
+            <WalletProvider wallets={wallets} autoConnect>
+                {props.children}
+                {/* <WalletModalProvider>
+                    <WalletMultiButton />
+                    <WalletDisconnectButton />
+                </WalletModalProvider> */}
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+export default WalletWrapper
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,17 @@ import 'tailwindcss/tailwind.css';
 
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query';
+import React from 'react';
+import WalletWrapper from '../components/WalletWrapper';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <WalletWrapper>
+        <Component {...pageProps} />
+      </WalletWrapper>
     </QueryClientProvider>
   )
 }
