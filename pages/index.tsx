8456aae4b2df8831f9251a2035f0f06d1e565f8b
Rui Xian <tayrx9988@gmail.com>
2022-04-02 13:14:17 +0800

    feat: upload idl

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,28 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
 
 const Home: NextPage = () => {
     return (
         <div>
-            
+            <div className="h-20 bg-blue-400 px-10 flex justify-between items-center">
+                <div>
+                    Somelogo
+                </div>
+
+                <WalletModalProvider>
+                    <WalletMultiButton />
+                </WalletModalProvider>
+            </div>
+
+            <div className="h-80 bg-yellow-500">
+
+            </div>
         </div>
     )
 }
